b6db5f64e9809eba049efdfa7831858d9db6e76b
Chu Cheuk Kiu <calvinchu26@gmail.com>
2017-10-06 10:03:19 +0800

    remove test.cpp separated modulo functions to new file

--- test.cpp
+++ test.cpp
@@ -62,24 +62,7 @@ int getLiveNeighbors(const int gameBoard[][GAMEBOARDSIZE], int x, int y) {
 	return neighbors;
 }
 
-int gcd(int a, int b){
-  if(a%b == 0)
-  return b;
-return gcd(b,a%b);
-}
 
-int m_inverse(const int TARGET, const int MOD_SPACE){
-  if(gcd(TARGET,MOD_SPACE)!=1){
-    cout<<"There is no such inverse."<<endl;
-    return 0;
-  }
-  for(int i = 1; i< MOD_SPACE; i++){
-    if((i*TARGET)%MOD_SPACE==1){
-      return i;
-    }
-  }
-  return -1;
-}
 
 int main(){
 
