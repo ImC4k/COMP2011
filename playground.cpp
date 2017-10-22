1b73f3df222c9aa2db9dfcdb674ffba153549247
Chu Cheuk Kiu <calvinchu26@gmail.com>
2017-10-22 22:43:04 +0800

    finally finished pa2.cpp (i think), bug check is required

--- playground.cpp
+++ playground.cpp
@@ -2,30 +2,9 @@
 
 using namespace std;
 
-const int BOARD_SIZE = 4;
-
-int getSmallestBlock(const int blocks[], int size) {
-  if(size == 0){
-    if(blocks[size] != 0){
-      return size + 1;
-    }
-    else return BOARD_SIZE + 1;
-  }
-  else{
-    int previous = getSmallestBlock(blocks, size - 1);
-    if(previous != BOARD_SIZE + 1){
-      return previous;
-    }
-    else if(blocks[size] != 0){
-      return size + 1;
-    }
-    else return BOARD_SIZE + 1;
-  }
-  return 0;
-}
-
 int main(){
-  int blocks[] = {0, 0, 0, 0};
-  cout<<getSmallestBlock(blocks, BOARD_SIZE);
+  int x = 2;
+  while(x--);
+  cout<<x<<endl;
   return 0;
 }
